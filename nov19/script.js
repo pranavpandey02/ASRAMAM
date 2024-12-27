334c4415014a761784b6d280dfad68ca0994c682
pranavpandey02 <pranavpandey479@gmail.com>
2024-12-27 20:18:49 +0530

    commiting nov19/

---
+++ nov19/script.js
@@ -0,0 +1,30 @@
+function display()
+{
+    console.log("hello world");
+}
+
+var h = document.getElementById("demo");
+
+// function sec()
+// {
+//     var name ="pranav";
+//     var course = "web dev";
+  
+//     h.innerHTML=(name + course);
+// }
+// var number= prompt("enter your number")
+
+// function cube(numa)
+// { 
+//     var number = prompt("enter your number");
+//     var result = number * number * number;
+//     h.innerHTML = result;
+// }
+
+
+function dis()
+{
+    h.style.color = "red";
+    h.style.backgroundColor="black";   
+    // h.style.padding=30px;
+}
