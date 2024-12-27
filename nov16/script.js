6fa1baf2cb3b52327e72afaa96bfe1420f81ad6b
pranavpandey02 <pranavpandey479@gmail.com>
2024-12-27 20:16:56 +0530

    nov16 commiting

---
+++ nov16/script.js
@@ -0,0 +1,37 @@
+// // == comapre only values and === value and data type
+// var num1 = 10;
+// var num2 = "10";
+// if(num1===num2)
+// {
+//     alert("both are equal ")
+// }
+// else {
+//     alert("both are not equal");
+// }
+
+
+
+//take 3 varibale for your name , age and course;
+// display it in alert, console and innerhtml in string Formmat
+var pname = "Pranav Pandey";
+// main.innerHTML=pname;
+
+
+
+var age = 30;
+
+
+var course = "full stack ";
+
+alert(course);
+
+var main = document.getElementById("hel");
+
+alert(pname+" "+age+" "+course);
+
+main.innerHTML=pname + " "+age+" " +course;
+
+console.log(pname+" "+age+" "+course);
+
+
+ 
