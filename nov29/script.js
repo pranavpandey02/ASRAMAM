b4fa64e9d5cbf6c302ec60bb126c3e041eb1acd3
pranavpandey02 <pranavpandey479@gmail.com>
2025-01-01 22:55:39 +0530

    commiting

---
+++ nov29/script.js
@@ -0,0 +1,27 @@
+var name1 = document.getElementById("demo1");
+var name2 = document.getElementById("demo2");
+var name3 = document.getElementById("demo3");
+
+function name()
+{
+    name1.innerHTML = "pranav";
+    alert("sudhanshu");
+    console.log("pranav");
+}
+name();
+
+const namee = function()
+{
+    name2.innerHTML = "Shiva";
+    alert("shiva");
+    console.log("shiva");
+}
+namee();
+
+var name3 = ()=>
+{
+    name3.innerHTML = "abhinav";
+    alert("abhinav");
+    console.log("abhinav");
+}
+name3();
