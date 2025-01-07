e70787728337ff8f88d9f9eb018463cd0673924c
pranavpandey02 <pranavpandey479@gmail.com>
2025-01-07 15:49:36 +0530

    commiting the value

---
+++ homeworknov19/script.js
@@ -0,0 +1,33 @@
+var h = document.getElementById("kya");
+var b = document.getElementById("btn");
+
+// function display()
+// {
+//     h.innerHTML=("hello ji ap kya kr rhe ho");
+//     h.style.color="red";
+//     h.style.backgroundColor = "black";
+//     h.style.display="inline";
+//     b.style.color = "blue"; 
+//     b.style.backgroundColor="green";
+//     b.style.padding="30px";
+//     b.style.borderRadius="5px";
+//     b.style.fontSize="30px";
+// }
+
+// taking a prompt nd calculted the cube;
+
+// function cube(num1)
+// {
+// var num1 =prompt("tell me about the number");   
+// var value = num1*num1*num1;
+// h.innerHTML=value;
+// }
+
+// taking a variable of two string nd print in innerhtml
+
+function two()
+{
+    var name = prompt("Plese enter your name");
+    var age = prompt("please enter your age");
+    h.innerHTML=(name + " "+ age);
+} 
