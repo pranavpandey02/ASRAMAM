ff1e0c9788ff4f35adad32aff0086deb407f1fd8
pranavpandey02 <pranavpandey479@gmail.com>
2025-01-10 17:43:08 +0530

    commting the value

---
+++ 10dec/script.js
@@ -0,0 +1,94 @@
+// // var age = 45;
+
+// // console.log(typeof age);
+// // var newAge = age.toString();
+// // console.log(typeof newAge);
+// // // its return a num to string;
+
+// // var data = "Javascript";
+// // var dataIn = data.length;
+// // console.log(dataIn);
+// // // its return a length of string;
+
+// // var data = "Javascript";
+// // var text ="is a very easy and simple language";
+// // var combined = data.concat(text);
+// // console.log(combined);
+// // var dataIn = data.length;
+// // console.log(dataIn);
+
+
+
+// // // its return charcter of given
+// // var data = "Javascript";
+// // var newChar = data.charAt(3);
+// // console.log(newChar);
+
+// // // to upper case
+
+// // var data = "Javascript";
+// // var UpperCase = data.toUpperCase();
+// // console.log(UpperCase)
+
+// // var lowercase = data.toLowerCase();
+// // console.log(lowercase)
+
+ // var newChar = data.newCharadd
+
+
+// let name ="  john doe  ";
+// let trimmedText = name.trim();
+// // its remove a white spaces in both side;
+// console.log(trimmedText);
+
+
+// // // spilting a string into an array;
+
+// const data = "this is a string which will spily into arry";
+
+// const splittedData = data.split(" ");
+
+// console.log(splittedData);
+
+
+// const somethings = "Javascript is a progrming langugae";
+// // slice extract the part of string , it takes 2 parameters 1st is the starting poistion and second is the ending postion and ending postion is not include
+
+// // slice 
+// let newData = somethings.slice(0,31);
+// // if you omit the 2ns parametrs it will slice the reaming string ;
+// console.log(newData);
+// newData = somethings.slice(14);
+
+// console.log(newData);
+// newData = somethings.slice(-13,-4);
+
+// // substring
+// // substring is a similar to slice but cannot take negative value;
+
+// newData = somethings.substring(0,4);
+// console.log(newData);
+
+
+// // substr
+// // here first parameter is postion and 
+// // 2ns paramter is lenth of string you want to extract
+
+// newData = somethings.substr(0,15);
+// console.log(newData);
+
+// // Replace method 
+// const myString = "ALL that giteter is not a data gold is prciouse";
+// // replace method only replace the first elements;
+
+// let newText = myString.replace("gold","silver")
+// console.log(newText);
+// Replace method
+
+// date 12 dec
+
+// let myData = "google is the gtreat place to work, google give man y aminties of the meployee";
+
+// let newData = myData.replace(/google/gi,"amazon");
+// // where g for global and i for case sensitive search.
+// console.log(newData);
